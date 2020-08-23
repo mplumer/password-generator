55b17c4132777a0a356a63a946c133de92491c7c
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-08-23 17:22:53 -0400

    fix issues and refactor code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,81 +1,106 @@
+// Assignment code here
 // Password Characters
-let alphabet = 'abcdefghijklmnopqrstuvwxyz';	
-let alphabetCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';	
-let numbers = '0123456789';	
-let specialChars = "!$%&'()*+,-./:;<=>?@[]^_`{|}~";	
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let alphabetCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let numbers = '0123456789'
+let specialChars = "!$%&'()*+,-./:;<=>?@[]^_`{|}~"
 let password = "";
+// Gnerate Password Function Start
 
-// get password length
-const getPasswordLength = () => {
-    let passwordLength = window.prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
-    if (passwordLength >= 8 && passwordLength <= 128) {
-        passwordLength = passwordLength
-    } else {
-        window.alert("Invalid entry please choose a number between 8 and 128! ");
-        getPasswordLength();
+const generatePassword = () => {
+
+
+    // get password length
+    const getPasswordLength = () => {
+        let passwordLength = window.prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+        if (passwordLength >= 8 && passwordLength <= 128) {
+
+            //creates empty array of length of prompt window input
+            passwordLength = Array.from({ length: passwordLength })
+
+
+        } else {
+            window.alert("Invalid entry please choose a number between 8 and 128! ");
+            getPasswordLength();
+        }
+        return passwordLength;
     }
-    return passwordLength;
+
+    let passwordLength = getPasswordLength();
+
+
+    // choose any or some of lowercase, uppercase, numers and special characters
+    const getCharTypes = () => {
+
+        // lowercase
+        let charLowerCase = window.prompt("Would you like your password to contain lowercase characters? Please type yes or no.").toLowerCase();
+        if (charLowerCase === "yes") { charLowerCase = alphabet }
+        else if (charLowerCase === "no") { charLowerCase = "" }
+        else {
+            window.alert("Please choose yes or no!");
+            getCharTypes();
+        }
+
+        //uppercase
+        let charUpperCase = window.prompt("Would you like your password to contain uppercase characters? Please type yes or no.").toLowerCase();
+        if (charUpperCase === "yes") { charUpperCase = alphabetCap }
+        else if (charUpperCase === "no") { charUpperCase = "" }
+        else {
+            window.alert("Please choose yes or no!");
+            getCharTypes();
+        }
+
+        //numbers
+        let charNumbers = window.prompt("Would you like your password to contain numbers? Please type yes or no.").toLowerCase();
+        if (charNumbers === 'yes') { charNumbers = numbers }
+        else if (charNumbers === 'no') { charNumbers = "" }
+        else {
+            window.alert("Please choose yes or no!");
+            getCharTypes();
+        }
+
+        //special characters
+        let charSpecial = window.prompt("Would you like your password to contain special characters? Please type yes or no.").toLowerCase();
+        if (charSpecial === "yes") { charSpecial = specialChars }
+        else if (charSpecial === "no") { charSpecial = "" }
+        else {
+            window.alert("Please choose yes or no!");
+            getCharTypes();
+        }
+
+        //takes array of all character types and converts them to one string
+        let finalPasswordChars = [charLowerCase, charUpperCase, charSpecial, charNumbers].join("")
+
+
+        //return to beginning of function if no valid selection are made.
+        if (!finalPasswordChars) {
+            window.alert("You must select at least one type of character for your password! Please try again!");
+            getCharTypes();
+        } else {
+            window.alert("Conditions accepted! Preparing to generate password now!")
+
+        }
+        return finalPasswordChars;
+
+
+    }
+
+
+    finalPasswordChars = getCharTypes();
+
+
+    //reset password before new one is generated
+    password = "";
+
+    //add character at random index in finalPasswordChars.length
+    for (i = 0; i < passwordLength.length; i++) {
+        let c = Math.floor(Math.random() * finalPasswordChars.length);
+        password += finalPasswordChars.charAt(c)
+
+    }
+    //Randomized Password
+    return password;
 };
-let passwordLength = getPasswordLength();
-
-// choose any or some of lowercase, uppercase, numers and special characters
-const getCharTypes = () => {
-    // lowercase
-    let charLowerCase = window.prompt("Would you like your password to contain lowercase characters? Please choose yes or no.").toLowerCase();
-    if (charLowerCase === "yes") { charLowerCase = alphabet }
-    else if (charLowerCase === "no") { charLowerCase = "" }
-    else {
-        window.alert("Please choose yes or no!");
-        getCharTypes();
-    }
-
-    //uppercase
-    let charUpperCase = window.prompt("Would you like your password to contain uppercase characters? Please choose yes or no.").toLowerCase();
-    if (charUpperCase === "yes") { charUpperCase = alphabetCap }
-    else if (charUpperCase === "no") { charUpperCase = "" }
-    else {
-        window.alert("Please choose yes or no!");
-        getCharTypes();
-    }
-
-    //numbers
-    let charNumbers = window.prompt("Would you like your password to contain numbers? Please type yes or no.").toLowerCase();
-    if (charNumbers === 'yes') { charNumbers = numbers }
-    else if (charNumbers === 'no') { charNumbers = "" }
-    else {
-        window.alert("Please type yes or no!");
-        getCharTypes();
-    }
-
-    //special characters
-    let charSpecial = window.prompt("Would you like your password to contain special characters? Please type yes or no.").toLowerCase();
-    if (charSpecial === "yes") { charSpecial = specialChars }
-    else if (charSpecial === "no") { charSpecial = "" }
-    else {
-        window.alert("Please choose yes or no!");
-        getCharTypes();
-    }
-
-
-    let finalPasswordChars = [charLowerCase, charUpperCase, charSpecial, charNumbers].join("")
-
-    return finalPasswordChars;
-
-
-};
-
-
-finalPasswordChars = getCharTypes();
-console.log(finalPasswordChars)
-
-
-/*const generatePassword = () => {
-  for (i=1; i<passwordLength.length; i++) {
-    let c = Math.floor(Math.random() * finalPasswordChars.length +1);
-    password += finalPasswordChars.charAt(c)
-  }
-  return password;
-};*/
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
